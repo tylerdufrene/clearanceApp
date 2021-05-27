dd1eb3633984e953f6fa3f35861c818129012e36
Tyler Dufrene <49526008+tylerdufrene@users.noreply.github.com>
2021-05-27 16:39:29 -0500

    settings

--- clearance_project/clearance_project/settings.py
+++ clearance_project/clearance_project/settings.py
@@ -84,13 +84,20 @@ WSGI_APPLICATION = 'clearance_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'clearance_products',
+    #     'USER': 'root',
+    #     'PASSWORD': '1122#Mysqldb',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clearance_products',
-        'USER': 'root',
-        'PASSWORD': '1122#Mysqldb',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': 'tylerdufrene$clearance_products',
+        'USER': 'tylerdufrene',
+        'PASSWORD': '1122#Pythonanywhere',
+        'HOST': 'tylerdufrene.mysql.pythonanywhere-services.com',
     }
 }
 
